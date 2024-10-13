c66683a09d669cf2d7e6dcd0d87539e13cdb86fa
Andrea Pierré <andrea_pierre@brown.edu>
2024-10-13 14:44:56 -0400

    remove license choices

--- tests/test_templating.py
+++ tests/test_templating.py
@@ -11,7 +11,7 @@ def test_template_copy():
         env_name = copier_vars.get("environment_name").get("default")
         path_list_to_check = [
             TEMPLATE_LOC.joinpath(env_name),
-            # TEMPLATE_LOC.joinpath("LICENSE"),
+            TEMPLATE_LOC.joinpath("LICENSE"),
             TEMPLATE_LOC.joinpath("README.md"),
             TEMPLATE_LOC.joinpath("pyproject.toml"),
         ]
@@ -38,26 +38,26 @@ def test_pyproject_templating():
         assert env_vars.get("project").get("name") == str(env_name)
 
 
-@pytest.mark.parametrize(
-    "project_license, license_str",
-    [
-        ("MIT", "The MIT License"),
-        ("GPL-3.0-or-later", "GNU GENERAL PUBLIC LICENSE"),
-        ("Apache-2.0", "Apache License"),
-        (
-            "Unlicense",
-            "This is free and unencumbered software released into the public domain.",
-        ),
-        ("None", None),
-    ],
-)
-def test_license_templating(project_license, license_str):
-    with GenerateTemplate(project_license=project_license):
-        license_loc = TEMPLATE_LOC.joinpath("LICENSE")
-        if license_str:
-            assert license_loc.exists()
-            with open(license_loc, "r") as fid:
-                file_content = fid.read()
-            assert license_str in file_content
-        else:
-            assert not license_loc.exists()
+# @pytest.mark.parametrize(
+#     "project_license, license_str",
+#     [
+#         ("MIT", "The MIT License"),
+#         ("GPL-3.0-or-later", "GNU GENERAL PUBLIC LICENSE"),
+#         ("Apache-2.0", "Apache License"),
+#         (
+#             "Unlicense",
+#             "This is free and unencumbered software released into the public domain.",
+#         ),
+#         ("None", None),
+#     ],
+# )
+# def test_license_templating(project_license, license_str):
+#     with GenerateTemplate(project_license=project_license):
+#         license_loc = TEMPLATE_LOC.joinpath("LICENSE")
+#         if license_str:
+#             assert license_loc.exists()
+#             with open(license_loc, "r") as fid:
+#                 file_content = fid.read()
+#             assert license_str in file_content
+#         else:
+#             assert not license_loc.exists()
